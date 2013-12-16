996e1e26514f073c91d31e5f70ca57376801bc7e
Georg Koller <georg.koller@gmx.net>
2013-12-16 10:20:29 +0100

    Initialize

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import Model.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Elve e = new Elve();
+        System.out.println(e.getName());
+    }
+}
---
+++ src/main/java/Model/Elve.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Elve {
+    private String name = "Georg";
+
+    public String getName() {
+        return name;
+    }
+}
