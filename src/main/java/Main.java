eeb5359e1909da04501641ae6b1f708f37cd7da4
Georg Koller <georg.koller@gmx.net>
2014-01-13 21:29:48 +0100

    turntables, y?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,27 @@ public class Main {
         Clock clock = new Clock();
         clock.start();
 
+        Belt inputA = new Belt(2, clock, "InputBelt A");
+        Belt inputB = new Belt(3, clock, "InputBelt B");
+        Belt[] belts = {inputA,inputB};
+
+
         Belt beltA = new Belt(2, clock, "Belt A");
-        Belt beltB = new Belt(3, clock, "Belt B");
+        Belt beltB = new Belt(2, clock, "Belt B");
         Belt beltC = new Belt(2, clock, "Belt C");
-        Belt[] belts = {beltA,beltB,beltC};
+
+        Sack c1 = new Sack(inputA,1,clock,1);
+        Sack c2 = new Sack(inputB,2,clock,2);
+//        Sack c1 = new Sack(beltA,1,clock,1);
+//        Sack c2 = new Sack(beltA,2,clock,2);
+//        Sack c3 = new Sack(beltB,3,clock,3);
+//        Sack c4 = new Sack(beltC,4,clock,4);
+
+        Turntable turnA = new Turntable(inputA,beltA,null,null,clock);
+//        Turntable turnB = new Turntable(inputB,beltA,c1,beltB);
+//        Turntable turnC = new Turntable(beltB,c2,c3,c4);
+
+        turnA.start();
 
         // initialize RedHattedElves
         int numReds = 5;
@@ -21,16 +38,10 @@ public class Main {
             redElves[i].start();
         }
 
-
-        Sack c1 = new Sack(beltA,1,clock,1);
-        Sack c2 = new Sack(beltA,2,clock,2);
-        Sack c3 = new Sack(beltB,3,clock,3);
-        Sack c4 = new Sack(beltC,4,clock,4);
-
         c1.start();
         c2.start();
-        c3.start();
-        c4.start();
+//        c3.start();
+//        c4.start();
 
         try {
             for(int i=0;i<redElves.length;i++) {
@@ -39,6 +50,8 @@ public class Main {
         } catch (InterruptedException ex) {}
 
         Gift g = new Gift("END",0);
+        inputA.insert(g);
+        inputB.insert(g);
         beltA.insert(g);
         beltB.insert(g);
         beltC.insert(g);
@@ -46,10 +59,11 @@ public class Main {
         try {
             c1.join();
             c2.join();
-            c3.join();
-            c4.join();
+//            c3.join();
+//            c4.join();
         } catch (InterruptedException ex) {}
 
+        turnA.requestStop();
         clock.requestStop();
 
         System.out.println("Theme Park closed:");
--- src/main/java/Model/Turntable.java
+++ src/main/java/Model/Turntable.java
@@ -1,5 +1,39 @@
 package model;
 
+import java.util.Objects;
+
 public class Turntable extends Thread {
 
+    private Object[] slots;
+    private boolean stop = false;
+    Clock c;
+
+    public Turntable(Object a, Object b, Object c , Object d, Clock clock) {
+        slots = new Object[4];
+        slots[0] = a;
+        slots[1] = b;
+        slots[2] = c;
+        slots[3] = d;
+        this.c = clock;
+    }
+
+    public void run() {
+        while(!stop){
+            turn();
+            try {
+                sleep( (int) (Math.random() * 1000));
+            }
+            catch (InterruptedException ex) {}
+        }
+    }
+
+    public boolean turn() {
+        int i = c.time%4;
+        System.out.println("Richtung: " + i++);
+        return true;
+    }
+
+    public void requestStop() {
+        stop = true;
+    }
 }
