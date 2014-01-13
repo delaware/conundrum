39620488e33c4fc32bc4ff57b5cd98c52f741570
Georg Koller <georg.koller@gmx.net>
2014-01-13 19:07:07 +0100

    runnable, yay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,41 +5,45 @@ public class Main {
     public static void main(String[] args) {
 
         Clock clock = new Clock();
-        Thread tc = new Thread(clock);
-        tc.start();
+        clock.start();
 
-        Belt beltA = new Belt(2, clock);
-        //Belt beltB = new Belt(2, clock);
+        Belt beltA = new Belt(3, clock);
+        Belt beltB = new Belt(4, clock);
 
         Sack c1 = new Sack(beltA,1,clock,1);
         Sack c2 = new Sack(beltA,2,clock,2);
-        //Sack c3 = new Sack(beltA,3,clock,3);
-        //Sack c4 = new Sack(beltA,4,clock,4);
+        Sack c3 = new Sack(beltB,3,clock,3);
+        Sack c4 = new Sack(beltB,4,clock,4);
 
-        RedHatElf p1 = new RedHatElf(1,beltA,10,clock);
-        //RedHatElf p2 = new RedHatElf(2,beltA,30,clock);
-        //RedHatElf p3 = new RedHatElf(3,beltB,30,clock);
+        RedHatElf p1 = new RedHatElf(1,beltA,clock);
+        RedHatElf p2 = new RedHatElf(2,beltA,clock);
+        RedHatElf p3 = new RedHatElf(3,beltB,clock);
 
         c1.start();
         c2.start();
-        //c3.start();
-        //c4.start();
+        c3.start();
+        c4.start();
         p1.start();
-        //p2.start();
-        //p3.start();
+        p2.start();
+        p3.start();
 
-        //beltA.insert(null);
-        //beltB.insert("*****");
         try {
             p1.join();
-            //p2.join();
-            //p3.join();
-            c1.join();
-            c2.join();
-            //c3.join();
-            //c4.join();
+            p2.join();
+            p3.join();
         } catch (InterruptedException ex) {}
 
-        tc.interrupt();
+        Gift g = new Gift("END",0);
+        beltA.insert(g);
+        beltB.insert(g);
+
+        try {
+            c1.join();
+            c2.join();
+            c3.join();
+            c4.join();
+        } catch (InterruptedException ex) {}
+
+        clock.requestStop();
     }
 }
